1921c4c9060e3c395c75d1b82079768bc19cd788
Nuno Freitas <nuno.freitas@losch.lu>
2024-11-20 15:16:42 +0000

    commit

---
+++ .next/static/chunks/app_dashboard_layout_tsx_d91b07._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_dashboard_layout_tsx_d91b07._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_dashboard_layout_tsx_d91b07._.js",
+  "chunks": [
+    "static/chunks/node_modules__pnpm_227123._.js",
+    "static/chunks/app_b0570c._.js"
+  ],
+  "source": "dynamic"
+});
